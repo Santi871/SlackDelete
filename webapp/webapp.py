7afd55e4a5ff9b69dc68a6035839c75309d5776d
Santi871 <santivegega@gmail.com>
2016-09-09 02:58:08 -0300

    added show_whitelist

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -29,7 +29,7 @@ def command():
         elif slack_request.command == '/sdunwhitelist':
             response = sd.unwhitelist_user(slack_request.team_domain, slack_request.text.split()[0],
                                            slack_request.user_id)
-        elif slack_request.command == 'sdwhitelisted':
+        elif slack_request.command == '/sdwhitelisted':
             response = sd.show_whitelist(slack_request.team_domain)
     else:
         return 'Invalid request token'
